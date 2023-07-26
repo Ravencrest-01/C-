1c9a3cf53a90908edb176ecb4fc1fc44c43823e5
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-07-26 12:45:37 +0530

    First Push

--- merge sort/mergeSort.cpp
+++ merge sort/mergeSort.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int merge(vector<int>& arr, int left, int mid, int right){
+// int merge(vector<int>& arr, int left, int mid, int right){
     
-}
+// }
 
-int mergeSort(vector<int>& arr, int left,int right){
-    if(left<right){
-        int mid = left+(right-left)/2;
-    }
+// int mergeSort(vector<int>& arr, int left,int right){
+//     if(left<right){
+//         int mid = left+(right-left)/2;
+//     }
 
-    mergeSort(arr,left,mid);
-    mergeSort(arr,mid+1,right);
+//     mergeSort(arr,left,mid);
+//     mergeSort(arr,mid+1,right);
     
-}
+// }
 
 
-int main()
-{
+// int main()
+// {
 
-    return 0;
-}
+//     return 0;
+// }
